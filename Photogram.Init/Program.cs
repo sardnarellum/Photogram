0bc6d8e234a97071a29178d7c86d7b4e67bf38d7
András Müller <sardnarellum@gmail.com>
2014-10-01 13:19:04 +0200

    Implementing login (register still in progress)

---
+++ Photogram.Init/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Photogram.Portal.Models;
+
+namespace Photogram.Init
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PhotogramEntities db = new PhotogramEntities();
+
+            Setup s = new Setup
+            {
+                Email = "info@andrasmuller.com",
+                MainTitle = new TextValue[]
+                {
+                    new TextValue
+                    {
+                        Text = "MÜLLER ANDRÁS FOTOGRÁFUS",
+                        Language = new Language
+                        {
+                            Code = "hun",
+                            Name = "magyar"
+                        }
+                    },
+
+                    new TextValue
+                    {
+                        Text = "ANDRÁS MÜLLER PHOTOGRAPHER",
+                        Language = new Language
+                        {
+                            Code = "eng",
+                            Name = "angol"
+                        }
+                    },
+
+                }
+            };
+
+            db.Setup.Add(s);
+
+            db.SaveChanges();
+        }
+    }
+}
