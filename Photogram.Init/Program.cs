3886f2e788c254498c62fed1c504ad4aece152bd
András Müller <sardnarellum@gmail.com>
2014-11-26 09:08:17 +0100

    github initial

--- Photogram.Init/Program.cs
+++ Photogram.Init/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Photogram.Portal.Models;
+using Photogram.WebApp.Models;
 
 namespace Photogram.Init
 {
@@ -11,39 +11,103 @@ namespace Photogram.Init
     {
         static void Main(string[] args)
         {
-            PhotogramEntities db = new PhotogramEntities();
+            Console.WriteLine("PHOTOGRAM ADATBÁZIS INICIALIZÁCIÓ" + Environment.NewLine);
 
-            Setup s = new Setup
+            try
             {
-                Email = "info@andrasmuller.com",
-                MainTitle = new TextValue[]
+                PhotogramEntities db = new PhotogramEntities();
+
+                var hun = new Language
+                {
+                    Code = "hun",
+                    Name = "magyar"
+                };
+
+                var eng = new Language
+                {
+                    Code = "eng",
+                    Name = "angol"
+                };
+
+                var s = new Setup
+                {
+                    Email = "info@andrasmuller.com",
+                    MainTitle = new TextValue[]
                 {
                     new TextValue
                     {
                         Text = "MÜLLER ANDRÁS FOTOGRÁFUS",
-                        Language = new Language
-                        {
-                            Code = "hun",
-                            Name = "magyar"
-                        }
+                        Language = hun
                     },
 
                     new TextValue
                     {
                         Text = "ANDRÁS MÜLLER PHOTOGRAPHER",
-                        Language = new Language
-                        {
-                            Code = "eng",
-                            Name = "angol"
-                        }
+                        Language = eng
                     },
 
                 }
-            };
+                };
 
-            db.Setup.Add(s);
+                db.Setup.Add(s);
 
-            db.SaveChanges();
+
+                var g = new Gallery
+                {
+                    Title = new TextValue[]
+                {
+                    new TextValue
+                    {
+                        Text = "Teszt galéria cím",
+                        Language = hun
+                    },
+
+                    new TextValue
+                    {
+                        Text = "Text gallery title",
+                        Language = eng
+                    }
+                },
+                    Type = GalleryType.Portfolio,
+                    Visible = true,
+                    Year = 2014,
+                    Position = 1,
+                    Description = new TextValue[]
+                {
+                    new TextValue
+                    {
+                        Text = "hun Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consectetur et lacus eget iaculis. Nam lobortis lorem quis odio aliquet vehicula. Ut justo tortor, auctor eget laoreet fermentum, varius id.",
+                        Language = hun
+                    },
+
+                    new TextValue
+                    {
+                        Text = "eng Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consectetur et lacus eget iaculis. Nam lobortis lorem quis odio aliquet vehicula. Ut justo tortor, auctor eget laoreet fermentum, varius id.",
+                        Language = eng
+                    }
+                }
+                };
+
+                db.Gallery.Add(g);
+
+
+                db.SaveChanges();
+
+                Console.WriteLine("Az adatbázis inicializációja sikeres volt.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Az adatbázis inicializálása közben a következő kivétel keletkezett:"
+                    + Environment.NewLine + ex.Message);
+
+                if (null != ex.InnerException)
+                    Console.WriteLine("Belső kivétel:" + Environment.NewLine + ex.InnerException.Message);               
+            }
+            finally
+            {
+                Console.WriteLine("A kilépéshez üss le egy billentyűt...");
+                Console.ReadKey();
+            }
         }
     }
 }
