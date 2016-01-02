f54e7ac18ba547c4d9b97e0a6866d1a435286af2
András Müller <sardnarellum@gmail.com>
2016-01-02 16:17:14 +0100

    Culture settings error handling hotfix. test project and unit tests added.

---
+++ Photogram.UnitTests/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Photogram.WebApp.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Globalization;
+
+namespace Photogram.UnitTests
+{
+    [TestClass]
+    public class LanguageMgmtTests
+    {
+        [TestMethod]
+        public void AcceptGoodParam()
+        {
+            CultureManagement.SetCulture("en-US");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CultureNotFoundException))]
+        public void HandleBadParam()
+        {
+            CultureManagement.SetCulture("argargar");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CultureNotFoundException))]
+        public void ZeroIntParam()
+        {
+            int i = 0;
+            CultureManagement.SetCulture(i);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void NullStrParam()
+        {
+            string str = null;
+            CultureManagement.SetCulture(str);
+        }
+    }
+}
