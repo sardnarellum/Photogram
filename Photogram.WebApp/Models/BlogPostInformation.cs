b25451d692f05405700cb25b41baf9d03c7dffd0
András Müller <sardnarellum@gmail.com>
2016-02-26 19:23:38 +0100

    Reordered properties. Non-functional changes.

--- Photogram.WebApp/Models/BlogPostInformation.cs
+++ Photogram.WebApp/Models/BlogPostInformation.cs
@@ -7,15 +7,15 @@ namespace Photogram.WebApp.Models
     {
         public int Id { get; internal set; }
 
-        public string Body { get; internal set; }
+        public string Title { get; internal set; }
 
         public string Lead { get; internal set; }
+
+        public string Body { get; internal set; }
 
         public DateTime Modified { get; internal set; }
 
         public ICollection<Tag> Tags { get; internal set; }
-
-        public string Title { get; internal set; }
 
         public bool Visible { get; internal set; }
     }
