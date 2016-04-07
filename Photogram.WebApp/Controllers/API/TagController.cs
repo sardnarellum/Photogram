0a9b671afc47412fa26777fdb2d67d78aa4c8d23
András Müller <sardnarellum@gmail.com>
2016-04-07 10:11:11 +0200

    Visual studio upgrade to 2015 Update 2. All packages added to version control.

---
+++ Photogram.WebApp/Controllers/API/TagController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Photogram.WebApp.Controllers.API
+{
+    public class TagController : BaseApiController
+    {
+        public IHttpActionResult Get(string searchTerm)
+        {
+            var tags = string.IsNullOrEmpty(searchTerm)
+                ? _db.Tag.Select(x => new { Text = x.Name })
+                    .OrderBy(x => x.Text).AsEnumerable()
+                : _db.Tag.Where(x => x.Name.Contains(searchTerm)).Select(
+                    x => new { Text = x.Name }).OrderBy(x => x.Text).AsEnumerable();
+
+            return Ok(tags);
+        }
+    }
+}
